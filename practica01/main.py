d98f79a837ca952f04bae7d3478005d2744143f1
alexbs01 <alex.becerra.suarez01@gmail.com>
2024-10-05 17:55:37 +0200

    Función de ecualización funciona

--- practica01/main.py
+++ practica01/main.py
@@ -1,12 +1,4 @@
 """
-equalizeIntensity
-
-Descripción: Expandir donde hay mucha información y comprimir donde hay poca.
-    round(pixel * numeroBins + 0.5)
-    O interpolar todos los valores de la imagen. Ver entre que bins se encuentra el valor y hacer una interpolación lineal.
-"""
-
-"""
 filterImage
 
 Descripción: Filtrar una imagen con un kernel de convolución.
@@ -19,12 +11,13 @@ import skimage as ski
 
 def main():
     
-    img = cv2.imread('imagenesPrueba/grays.png', cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread('imagenesPrueba/eq0.png', cv2.IMREAD_GRAYSCALE)
     img = ski.util.img_as_float(img)
 
     print(img)
     print(img.shape)
-    imgOut = f.adjustIntensity(img, [0.25, 0.55], [0.1, 1.])
+    #imgOut = f.adjustIntensity(img, [0.4, 0.6], [0., 1.])
+    imgOut = f.equalizeIntensity(img, 1000)
 
     print(min(imgOut.flatten()), max(imgOut.flatten()))
 
