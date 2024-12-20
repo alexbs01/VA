0caf18eaa685b39787287f3f7697c7fb22a02825
alexbs01 <alex.becerra.suarez01@gmail.com>
2024-12-20 14:00:20 +0100

    Se podría decir que la práctica está terminada

--- practica02/main.py
+++ practica02/main.py
@@ -1,10 +1,6 @@
 import functions as f
-import cv2
-import skimage as ski
-import numpy as np
 import utils
 import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
 import os
 
 def main():
@@ -16,15 +12,16 @@ def main():
     #imgs = imgs[len(imgs)//2:]
 
     for img in imgs:
-        img = mpimg.imread('practica02/img/' + img)
-        imgOut = f.detectField(img)
+        image = mpimg.imread('practica02/img/' + img)
+        imgOut = f.detectField(image)
         
         lines = f.findGrassLines(imgOut)
         playersContours = f.findPlayers(imgOut)
         imgOut = utils.drawGrassLines(imgOut, lines)
         imgOut = utils.drawPlayers(imgOut, playersContours)
-
-        utils.show_imgs([img, imgOut])
+        name = "practica02/out/" + img 
+        utils.show_imgs([image, imgOut])
+        mpimg.imsave(name, imgOut)
         
 
 if __name__ == "__main__":
